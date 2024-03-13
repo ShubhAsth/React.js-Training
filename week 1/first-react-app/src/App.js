4bb8e726ce2f4435cceefd680536c720e2e34fef
ShubhAsth <shubham.asthana@tothenew.com>
2024-03-13 21:38:29 +0530

    fixing error

--- week 1/first-react-app/src/App.js
+++ week 1/first-react-app/src/App.js
@@ -1,10 +1,120 @@
-import './App.css';
-import { FORM } from './components/form.js'
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const [nameInput, setNameInput] = useState("");
+  const [priceInput, setPriceInput] = useState("");
+  const [descInput, setDescInput] = useState("");
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (!nameInput || nameInput.trim() === "") {
+      alert("Please enter a valid name.");
+      return;
+    }
+    if (!priceInput || priceInput < 0) {
+      alert("Please enter a valid Price.");
+      return;
+    }
+
+    if (!descInput || nameInput.trim() === "") {
+      alert("Please enter a valid Description.");
+      return;
+    }
+
+    setData([
+      ...data,
+      {
+        id: data.length + 1,
+        productName: nameInput,
+        price: priceInput,
+        desc: descInput,
+      },
+    ]);
+
+    setNameInput("");
+    setPriceInput("");
+    setDescInput("");
+  };
+
+  const handleDelete = (id) => {
+    setData(prevData => {
+      const filteredData = prevData.filter(item => item.id !== id);
+      
+      const updatedData = filteredData.map((item, index) => ({
+        ...item,
+        id: index + 1
+      }));
+      
+      return updatedData;
+    });
+  };
+
   return (
     <div className="App">
-      <FORM/>
+      <div>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello World</h1>
+      </div>
+      <div className="form-container">
+        <h3>Enter Product Data:</h3>
+        <form onSubmit={handleFormSubmit}>
+          <div className="form-row">
+            <label>Product Name:</label>
+            <input
+              type="text"
+              value={nameInput}
+              onChange={(e) => setNameInput(e.target.value)}
+            />
+          </div>
+
+          <div className="form-row">
+            <label>Product Price:</label>
+            <input
+              type="number"
+              value={priceInput}
+              onChange={(e) => setPriceInput(e.target.value)}
+            />
+          </div>
+
+          <div className="form-row">
+            <label>Enter Description:</label>
+            <input
+              type="text"
+              value={descInput}
+              onChange={(e) => setDescInput(e.target.value)}
+            />
+          </div>
+
+          <button type="submit">Save Data</button>
+        </form>
+        <table>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Description</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item) => (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.productName}</td>
+                <td>{item.price}</td>
+                <td>{item.desc}</td>
+                <td><button className="delete-button" onClick={() => handleDelete(item.id)}>x</button></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
